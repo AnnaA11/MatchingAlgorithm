da8016a19fd567294e27619da5be4ebcd48eaae1
AnnaA11 <33154492+AnnaA11@users.noreply.github.com>
2017-11-25 12:39:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ bool path_M_augmenting(ED::Graph const & M, std::vector <ED::NodeId> const & pat
 // so we compute in each partition class the M augmenting path
 
 //Warning: runtime OK but dont work correctly
-bool find_M_agumenting_path_inclass(ED::Graph const & Tree, ED::Graph const & M, std::vector <bool> & visited, UF::UnionFind const & Partition, std::vector <ED::NodeId> & p, ED::NodeId start, ED::NodeId end) {
+bool find_M_agumenting_path_inclass(ED::Graph const & Tree, ED::Graph const & M, std::vector <bool> & visited, UF::UnionFind & Partition, std::vector <ED::NodeId> & p, ED::NodeId start, ED::NodeId end) {
 	
 	visited.at(start)=1;
 	p.push_back(start);
@@ -50,7 +50,7 @@ bool find_M_agumenting_path_inclass(ED::Graph const & Tree, ED::Graph const & M,
 			return 1;
 		}
 		else if(Partition.equals(neighbor_id, start) && visited.at(neighbor_id)!=0) {
-			if(Partition.equals(M.node(neighbor_id).neighbors().at(0),start) {
+			if(Partition.equals(M.node(neighbor_id).neighbors().at(0),start)) {
 				p.push_back(neighbor_id);
 				if(find_M_agumenting_path_inclass(Tree,M,visited,Partition,p,M.node(neighbor_id).neighbors().at(0),end)==1) return 1;
 				p.pop_back();
@@ -62,7 +62,7 @@ bool find_M_agumenting_path_inclass(ED::Graph const & Tree, ED::Graph const & M,
 }
 
 //Warning: dont work correctly
-std::vector <ED::NodeId> find_M_agumenting_path(ED::Graph const & M, ED::Graph const & Tree, UF::UnionFind const & Partition, std::vector <ED::NodeId> const & p) {
+std::vector <ED::NodeId> find_M_agumenting_path(ED::Graph const & M, ED::Graph const & Tree, UF::UnionFind & Partition, std::vector <ED::NodeId> const & p) {
 	int start=0;
 	int end=0;
 	int k=Partition.find(p.at(0));
@@ -117,10 +117,10 @@ std::vector <ED::NodeId> DFS(ED::Graph const & T, const int size,  ED::NodeId v,
 
 //ToDo Delete
 //checks if v has a neighbour w with w\notin Odd(T), if not it returns v
-ED::NodeId check_neighbors_of_node(ED::NodeId v, ED::Graph const & G, std::vector <int> const & Partition) {
+ED::NodeId check_neighbors_of_node(ED::NodeId v, ED::Graph const & G, UF::UnionFind & Partition) {
 	auto const & node = G.node(v);
 	for (auto const & neighbor_id : node.neighbors()) {
-		if(Partition.position_in_tree(neighbor_id)!=2 && Partition.equals(neighbor_id, v)==0 {
+		if(Partition.position_in_tree(neighbor_id)!=2 && Partition.equals(neighbor_id, v)==0) {
 			return neighbor_id;
 		}
 	}
@@ -162,7 +162,6 @@ void update_partition_edges(ED::Graph G, std::vector <ED::NodeId> path, UF::Unio
 	
 	int k=Partition.find(path.at(0));
 	int current=k;
-	ED::NodeId node;
 	
 	//Position in Tree, 0 for even 1 for odd
 	int position=0;
@@ -176,7 +175,7 @@ void update_partition_edges(ED::Graph G, std::vector <ED::NodeId> path, UF::Unio
 					//update "Edges"
 					auto const & node = G.node(i);
 					for (auto const & neighbor_id : node.neighbors()) {
-						Edges.push_back((i,neighbor_id));
+						Edges.push_back(std::make_pair(i,neighbor_id));
 					}
 				}
 				position=1-position;
@@ -189,6 +188,47 @@ void update_partition_edges(ED::Graph G, std::vector <ED::NodeId> path, UF::Unio
 	//change the position of the partition in tree to even
 	Partition.change_position_even(k);
 }
+
+
+//grow tree corresponds to one iteration of the while loop in the perfect matching algorithm
+int grow_tree (ED::Graph const & G, ED::Graph & M, ED::NodeId const root, ED::Graph & Tree, UF::UnionFind & Partition, ED::NodeId const start, ED::NodeId end, std::vector <std::pair<ED::NodeId, ED::NodeId>> & Edges) 
+{	ED::NodeId even;
+	if(Partition.position_in_tree(end)==0 && check_node_M_exposed(end,M)){
+		Tree.add_edge(end,start);
+		Partition.change_position_odd(start);
+		//agument_M(M,find_M_agumenting_path(M,Tree,Partition,DFS(Tree, Tree.num_nodes(),root,end))); // ToDO:Implement function correctly
+		agument_M(M,DFS(Tree, Tree.num_nodes(),root,end));
+		return 1;													//returns 1 if an M augmenting path is found
+	}
+			
+	else if(Partition.position_in_tree(end)==0){
+		//update Tree and Positions in Tree
+		even=(M.node(end)).neighbors().at(0);
+		Tree.add_edge(start,end);  
+		Tree.add_edge(end,even);
+		Partition.change_position_even(even);
+		Partition.change_position_odd(end);
+				
+		//update "Edges" by adding all edges incident to the vertex "even" 
+		auto const & node = G.node(even);
+		for (auto const & neighbor_id : node.neighbors()) {
+			if(neighbor_id!=end) Edges.push_back(std::make_pair(even,neighbor_id));
+		}
+		return 0;													//returns 0 if an edge was added to T
+	}
+			
+	else {
+		//update "Partition" and update "Edges" by adding every edge incident to an odd vertex 
+		//Warning: we do not check if this edge already exists
+		//to avoid double edges we could check if start and end have the same Partitionclass
+		update_partition_edges(G,DFS(Tree, Tree.num_nodes(),start,end), Partition, Edges);
+		Tree.add_edge(start,end);
+		return 0;													//returns 0 if an odd cycle was found
+	}		
+	return 2;														//returns 2 if the tree is frustrated
+}
+
+
 
 //builds up a tree and returns 0 if M is not perfect, 1 if M is alreday perfect and 2 if the tree is frustrated
 int build_up_tree(ED::Graph const & G, ED::Graph & M) {
@@ -212,16 +252,17 @@ int build_up_tree(ED::Graph const & G, ED::Graph & M) {
 	else {
 		auto const & node = G.node(root);
 		for (auto const & neighbor_id : node.neighbors()){
-			Edges.push_back((root, neighbor_id));
+			Edges.push_back(std::make_pair(root, neighbor_id));
 		}
+		
 		while(Edges.size()>0 && i==0) {
-			start=Edges.at(Edges.size-1).first;
-			end=Edges.at(Edges.size-1).second;
-			Edges.pop_back();
+			start=Edges.at(Edges.size()-1).first;
+			end=Edges.at(Edges.size()-1).second;
 			//if "end" has odd distance to the root, we don't consider this edge
 			//if "end" becomes even after some shrinking operation, this edge will be added to "Edges" during the shrinking
 			if(Partition.position_in_tree(end)!=2) i=grow_tree(G,M,root,Tree,Partition,start,end,Edges); 
 		}
+		
 		if(i==1){
 			if(check_M_perfect(M)) return 1;
 			else return 0;
@@ -231,86 +272,13 @@ int build_up_tree(ED::Graph const & G, ED::Graph & M) {
 }
 
 
-
-
-//grow tree corresponds to one iteration of the while loop in the perfect matching algorithm
-int grow_tree (ED::Graph const & G, ED::Graph & M, ED::NodeId const root, ED::Graph & Tree, UF::UnionFind & Partition, ED::NodeId const start, ED::NodeId end,std::vector <std::pair<ED::NodeId, ED::NodeId>> Edges) 
-{
-	ED::NodeId node;
-	if(Partition.position_in_tree(end)==0 && check_node_M_exposed(end,M)){
-		Tree.add_edge(end,start);
-		Partition.change_position_odd(start);
-		//agument_M(M,find_M_agumenting_path(M,Tree,Partition,DFS(Tree, Tree.num_nodes(),root,end))); // ToDO:Implement function correctly
-		agument_M(M,DFS(Tree, Tree.num_nodes(),root,end));
-		return 1;													//returns 1 if an M augmenting path is found
-	}
-			
-	else if(Partition.position_in_tree(end)==0){
-		//update Tree and Positions in Tree
-		Tree.add_edge(start,end);  
-		Tree.add_edge(end,(M.node(end)).neighbors().at(0));
-		Partition.change_position_even((M.node(end)).neighbors().at(0));
-		Partition.change_position_odd(end);
-				
-		//update "Edges" by adding all edges incident to the vertex  which is mached to "end" 
-		auto const & node = G.node(end);
-		for (auto const & neighbor_id : node.neighbors()) {
-			if(neighbor_id!=start) Edges.push_back((end,neighbor_id));
-		}
-		return 0;													//returns 0 if an edge was added to T
-	}
-			
-	else {
-		//update "Partition" and update "Edges" by adding every edge incident to an odd vertex 
-		//Warning: we do not check if this edge already exists
-		//to avoid double edges we could check if start and end have the same Partitionclass
-		update_partition_edges(G,DFS(Tree, Tree.num_nodes(),start,end), Partition, Edges);
-		Tree.add_edge(start,end);
-		return 0;													//returns 0 if an odd cycle was found
-	}		
-	return 2;														//returns 2 if the tree is frustrated
-}
-
-//ToDo: delete
-/*
-int build_up_tree(ED::Graph const & G, ED::Graph & M, ED::NodeId const start) {
-	ED::Graph Tree{G.num_nodes()};
-	std::vector <int> T(G.num_nodes());
-	std::vector <ED::NodeId> T_even;
-	//to simulate the shrinking we use partition classes
-	std::vector <int> Partition(G.num_nodes());
-	std::vector <std::vector <ED::NodeId>> Partition_classes(G.num_nodes());
-	
-	for(unsigned int i=0; i<G.num_nodes(); i++){
-		Partition.at(i)=i;		//at the beginning each vertex has its own partition class 
-		Partition_classes.at(i).push_back(i);
-	}
-	
-	T_even.push_back(start);
-	T[start]=1;
-	
-	int i=0;
-
-	while(i==0){
-		i=grow_tree(G,M,start, Tree, T, T_even, Partition, Partition_classes);
-	}
-	if(i==1){
-		if(check_M_perfect(M)==1){
-			return 1;
-		}
-		else return 0;
-	}
-	else return 2;
-}*/
-
 ED::Graph perfect_Matching(ED::Graph const &G) {
 	ED::Graph M{G.num_nodes()};
-	ED::NodeId start=0;
 	if(G.num_nodes()==0 || G.num_nodes()==1){ 
 		std::cout<< "G is empty or has only one vertex";
 		return M;
 	}
-	int i=build_up_tree(G, M, start);
+	int i=build_up_tree(G, M);
 	if(i==1) {
 		std::cout<< "M has perfect matching";
 		return M;
@@ -344,24 +312,27 @@ int main()
 	ED::Graph g2{6};
 	ED::Graph g3{10};
 	ED::Graph g4{10};
-
+	
+	g1.add_edge(6,0);
 	g1.add_edge(0,1);
 	g1.add_edge(2,3);
 	g1.add_edge(4,5);
-	g1.add_edge(6,0);
-	g1.add_edge(1,4);
+	
 	g1.add_edge(1,2);
 	g1.add_edge(2,7);
 	g1.add_edge(4,3);
 	g1.add_edge(3,5);
+	g1.add_edge(1,4);
 	
-	g2.add_edge(0,4);
+	
 	g2.add_edge(1,2);
 	g2.add_edge(3,0);
 	g2.add_edge(4,5);
 	g2.add_edge(0,1);
 	g2.add_edge(2,3);
+	g2.add_edge(0,4);
 	
+	g3.add_edge(0,9);
 	g3.add_edge(0,1);
 	g3.add_edge(1,2);
 	g3.add_edge(2,3);
@@ -371,12 +342,11 @@ int main()
 	g3.add_edge(6,7);
 	g3.add_edge(7,8);
 	g3.add_edge(8,1);
-	g3.add_edge(0,9);
-
+	
 
 	std::cout<< perfect_Matching(g1);
 	std::cout<< perfect_Matching(g2);
-	std::cout<< perfect_Matching(g4);
+	std::cout<< perfect_Matching(g3);
 
    return 1;
 }

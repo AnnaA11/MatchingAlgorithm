19bc4acb3beb2c6aa82a526a00c84bd8c082b49b
AnnaA11 <33154492+AnnaA11@users.noreply.github.com>
2017-11-25 14:20:24 +0100

    Update union_find.cpp

--- union_find.cpp
+++ union_find.cpp
@@ -20,17 +20,23 @@ public:
 	UnionFind(std::size_t size);
 
 	//Find the representative (root) of i's partition class
-	int find(int i);
+	int find(unsigned int i);
 
 	//Compare if i and j belong to the same partition class
 	bool equals(int i, int j);
 
 	//Unite the partition classes of i and j
 	void unite(int i, int j);
+	
+	int get_position_in_tree(int i);
+	
+	void set_position_even(int i);
+	
+	void set_position_odd(int i);
 
 private:
 	//Internally save the partition classes as trees in forest
-	std::vector<int> _forest;
+	std::vector<std::vector <int>> _forest;
 
 	//Height of every vertex in the respective tree
 	//@warning: Note that ranks are only kept up to date for roots of trees!
@@ -47,34 +53,51 @@ Upon initalization, each number is it's own partition class, meaning each number
 All ranks are zero
 */
 UnionFind::UnionFind(std::size_t size)
-	: _forest(),
+	: _forest(size),
 	_ranks(size, 0),
 	_size(size)
 	{
-		_forest.reserve(size);
-		for (int i = 0; i<size; i++){
-			_forest.push_back(i);
+		for (unsigned int i = 0; i<size; i++){
+			_forest.at(i).push_back(i);
+			//Position in tree
+			_forest.at(i).push_back(0);
+			//apex of pseudonode
+			//warning: position and apex only updated for the root of a pseudonode
+			_forest.at(i).push_back(-1);
 		}
 	}
+
 
 /*
 Find the root of the partition class to which number i belongs
 Checks if index i is in {0, ..., n-1}, throws std::out_of_range otherwise
 Returns the root of the partition class
 */
-int UnionFind::find(int i){
+int UnionFind::find(unsigned int i){
 	if (i >= _size || i < 0){
 		throw std::out_of_range("UnionFind::find");
 	}
-	int & current = i;
-	int & parent = _forest[i];
+	int current = i;
+	int parent = _forest.at(i).at(0);
 	while (parent != current){
 		current = parent;
-		parent = _forest[current];
+		parent = _forest.at(current).at(0);
 	}
+	_forest.at(i).at(0)=current;
 	return current;
 }
 
+
+int UnionFind::get_position_in_tree(int i) {
+	return _forest.at(find(i)).at(1);
+}
+
+void UnionFind::set_position_even(int i) {
+	_forest.at(find(i)).at(1)=1;
+}
+void UnionFind::set_position_odd(int i) {
+	_forest.at(find(i)).at(1)=2;
+}
 /*
 Checks if i and j belong to the same partition class or not
 */
@@ -95,10 +118,10 @@ void UnionFind::unite(int i, int j){
 		throw std::logic_error("UnionFind::unite cannot be executed for two equal partition classes.");
 	}
 	if (_ranks[root_i] > _ranks[root_j]){
-		_forest[root_j] = root_i;
+		_forest.at(root_j).at(0) = root_i;
 	}
 	else {
-		_forest[root_i] = root_j;
+		_forest.at(root_i).at(0) = root_j;
 		if (_ranks[root_i] == _ranks[root_j]){
 			_ranks[root_j] += 1;
 		}
@@ -107,5 +130,3 @@ void UnionFind::unite(int i, int j){
 }//namespace UF
 
 //ToDo: To be removed
-int main()
-{ return 0; }
